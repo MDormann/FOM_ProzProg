06d4bd8745c4333bed9621193d0467c561397623
Markus Dormann <Markus@Dormann.it>
2021-10-23 23:21:45 +0200

    Task 10-13 added

---
+++ Uebung_12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uebung_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("12. Aufgabe");
+            Console.ResetColor();
+            
+            Console.WriteLine(@"Schreiben Sie ein Programm, das mithilfe einer Schleife herausfindet, welche natürliche Zahl die kleinste fünfstellige Quadratzahl hat.");
+            
+            int counter = 0;
+            double result = 0;
+            do
+            {
+                counter++;
+                result = Math.Pow(counter, 2);
+            } while (result < 10000);
+            
+            Console.WriteLine("Ergebnis: natürliche Zahl '" + counter + "' Quadratzahl '" + result + "'");
+        }
+    }
+}
