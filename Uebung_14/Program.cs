9bffba9917d305e63e1c3da128146f2485a4f939
Markus Dormann <Markus@Dormann.it>
2021-11-05 22:01:42 +0100

    Task 14-16 added

---
+++ Uebung_14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uebung_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("14. Aufgabe");
+            Console.ResetColor();
+            
+            Console.WriteLine(@"Schreiben Sie ein Programm, das vom Benutzer für jede volle Stunde des Tages einen Temperaturwert entgegennimmt und in einer Liste abspeichert.");
+
+            List<decimal> temperatureWerte = new List<decimal>();
+
+            for (int i = 0; i < 24; i++)
+            {
+                Console.WriteLine($"Bitte geben Sie den Temperatur-Wert für Stunde {i} ein.");
+                decimal temperaturWert = decimal.Parse(Console.ReadLine()!);
+                
+                temperatureWerte.Add(temperaturWert);
+            }
+
+            for (int i = 0; i < 24; i++)
+            {
+                Console.WriteLine($"Eingabe für Stunde {i}: {temperatureWerte[i]}");
+            }
+        }
+    }
+}
---
+++ Uebung_15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uebung_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("15. Aufgabe");
+            Console.ResetColor();
+            
+            Console.WriteLine(@"Schreiben Sie ein Programm, das vom Benutzer zwölf Monatstemperaturen entgegennimmt, in einer passenden Liste speichert und dann den Jahresmittelwert ausgibt.");
+            
+            List<decimal> temperatureWerte = new List<decimal>();
+            decimal summe = 0;
+            
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"Bitte geben Sie den Monat {i} ein.");
+                decimal temperaturWert = decimal.Parse(Console.ReadLine()!);
+                
+                temperatureWerte.Add(temperaturWert);
+                summe += temperaturWert;
+            }
+
+            
+            Console.WriteLine($"Der Jahresmittelwert beträgt: {summe / 12}");
+            
+        }
+    }
+}
